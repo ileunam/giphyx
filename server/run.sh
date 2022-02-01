4612b1aa12063a5f8faeb9c58b7e3654f4118f08
Sebastian Mora Espinoza <sebmora@gmail.com>
2022-02-01 18:01:53 -0300

    feat(dockerfile): added scripts for jar files running

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,5 @@
-
-
-
-
-java -jar
+java -jar ms-eureka-0.0.1-SNAPSHOT.jar &
+java -jar gif-app-0.0.1-SNAPSHOT.jar &
+java -jar ms-gateway-0.0.1-SNAPSHOT.jar &
+java -jar ms-oauth-0.0.1-SNAPSHOT.jar &
+java -jar ms-users-0.0.1-SNAPSHOT.jar &
